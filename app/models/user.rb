668da7c7fad46296592577de8cf5db78c9011a45
nread <nathaniel_read@taylor.edu>
2013-09-26 04:50:10 +0000

    Having Rspec issues, cannot load.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   #attr_accessor :username, :password, :confirmation
   has_secure_password
+  
+  validates :username, presence: true
+  validates_uniqueness_of :username
+  validates_length_of :username, :maximum => 25
+  
+  validates :email, presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
