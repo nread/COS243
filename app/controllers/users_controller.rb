ca1d77a41e638ae6c95cebc68929bebecbdc68e4
nread <nathaniel_read@taylor.edu>
2013-10-02 19:03:51 +0000

    Fixed database issues.  Migrations file had commas seperating the different columns.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,44 @@ class UsersController < ApplicationController
 	def new
 		@user = User.new
 	end
+  
   def create
-    permitted_params = params.require(:user).permit(:username, :password, :password_confirmation)
 		@user = User.new(permitted_params)
-    #@user = User.new(:username => params[:username],:password => params[:password])
-    #render 'new'
     if @user.save then
 			redirect_to @user #type of response that the server can give to the browser.
 		else
 			render 'new'
 		end
   end
+    
+  def show
+    @user = User.find(params[:id])
+  end
+  def index
+    @users = User.all
+  end
+    
+  def edit
+    @user = User.find(params[:id])
+  end
+    
+  def update
+    @user = User.find(params[:id])
+    if @user.update(permitted_params) then
+			redirect_to @user
+		else
+      render 'edit'
+		end
+  end
+    
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to user_path
+  end  
+    
+  private 
+    def permitted_params
+      params.require(:user).permit(:username, :password, :password_confirmation, :email)
+    end
 end
