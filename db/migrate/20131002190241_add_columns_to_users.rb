7149ca57f4c379e5be6dffe6d69ca38b39cb1d7a
nread <nathaniel_read@taylor.edu>
2013-10-04 01:16:40 +0000

    Passing first 29 tests

---
+++ db/migrate/20131002190241_add_columns_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :username, :string
+    add_column :users, :password_digest, :string
+    add_column :users, :email, :string
+  end
+end
