63f26b958c477dda3abd5c56623ac9f3f49df516
nread <nathaniel_read@taylor.edu>
2013-10-20 20:52:52 +0000

    trying to switch to master branch

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+    
+  end
+  
+  def create
+    #@user = nil
+    @user = User.find_by(username: params[:username])
+    @user = 
+    if @user && @user.authenticate(params[:password])
+      cookies.signed[:user_id] = @user.id
+      flash[:success] = "#{@user.username} logged on"
+      redirect_to @user
+    else
+      flash.now[:danger] = "Invalid username or password"
+      render 'new'
+    end
+  end
+    
+  def destroy
+    cookies.signed[:user_id] = nil
+  end
+end
